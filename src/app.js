d1a0a3033ad64abac7979973dc894db47e3f77de
josdugan <josdugan@gmail.com>
2021-05-31 11:26:33 -0400

    Update dependencies; remove unused imports

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import './firebase/firebase';
 
 const store = configureStore();
 
