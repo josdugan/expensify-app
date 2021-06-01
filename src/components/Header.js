df9346fe095d67fd5aeee421728e8c87f858394b
josdugan <josdugan@gmail.com>
2021-06-01 15:10:51 -0400

    Remove HelpPage

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ const Header = ({ startLogout }) => (
         <h1>Expensify</h1> 
         <NavLink activeClassName="is-active" to="/dashboard">Dashboard</NavLink> |&nbsp;
         <NavLink activeClassName="is-active" to="/create">Add Expense</NavLink> |&nbsp;
-        <NavLink activeClassName="is-active" to="/help">Help</NavLink>
         <button onClick={startLogout} >Logout</button>
     </header>
 );
